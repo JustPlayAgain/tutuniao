80a874b9e851597718a0b40650c05964458af1b5
yuguangchao <yuguangchao@secoo.com>
2018-12-24 09:52:04 +0800

    init Project

---
+++ src/main/java/com/tutuniao/tutuniao/TutuniaoApplication.java
@@ -0,0 +1,14 @@
+package com.tutuniao.tutuniao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutuniaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutuniaoApplication.class, args);
+    }
+
+}
+
