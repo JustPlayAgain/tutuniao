c69e94030ad290407832564d64ae7a8b9ec1a713
yuguangchao <yuguangchao@secoo.com>
2019-01-06 19:48:43 +0800

    httpClient add Header

--- src/main/java/com/tutuniao/tutuniao/schedule/ScheduledService.java
+++ src/main/java/com/tutuniao/tutuniao/schedule/ScheduledService.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.tutuniao.tutuniao.entity.Banner;
 import com.tutuniao.tutuniao.entity.IndexObject;
 import com.tutuniao.tutuniao.util.HttpClientUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @Service
 public class ScheduledService {
@@ -26,17 +28,23 @@ public class ScheduledService {
     public static IndexObject buildIndex(){
         try {
             IndexObject indexObject = new IndexObject();
-            String dataAsStringFromUrl = HttpClientUtil.getDataAsStringFromUrl(academyArtUrl);
+            HashMap<String, String> headerParams = new HashMap<>();
+            headerParams.put("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36");
+            headerParams.put("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+            headerParams.put("Accept-Encoding","gzip, deflate");
+            String dataAsStringFromUrl = HttpClientUtil.getDataAsStringFromUrlWithHeader(academyArtUrl,null,500,headerParams);
             Document doc = Jsoup.parse(dataAsStringFromUrl);
 
             // 获取 banner
             String banner = doc.getElementsByClass("banner").toString();
-            indexObject.setBanners(urlAddHttp(banner));
-
-            String content = doc.getElementsByClass("container").toString();
-            indexObject.setContent(urlAddHttp(content));
-
-            return indexObject;
+            if(StringUtils.isNoneBlank(banner)){
+                indexObject.setBanners(urlAddHttp(banner));
+                String content = doc.getElementsByClass("container").toString();
+                if(StringUtils.isNoneBlank(content)) {
+                    indexObject.setContent(urlAddHttp(content));
+                    return indexObject;
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
