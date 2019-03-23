c7c1b494d718ba4a5a932d5f59f90c4dac899f63
yuguangchao <yuguangchao@secoo.com>
2019-03-23 16:58:22 +0800

    多余引用

--- src/main/java/com/tutuniao/tutuniao/service/impl/GuoMeiTemplateServiceImpl.java
+++ src/main/java/com/tutuniao/tutuniao/service/impl/GuoMeiTemplateServiceImpl.java
@@ -20,7 +20,6 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.io.IOException;
 import java.io.InputStream;
