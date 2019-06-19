b26815cc97d17505f98eab40f5d8fe4c3e60baa0
yuguangchao <yuguangchao@secoo.com>
2019-06-19 16:34:38 +0800

    add download image

--- src/main/java/com/tutuniao/tutuniao/common/Constant.java
+++ src/main/java/com/tutuniao/tutuniao/common/Constant.java
@@ -6,4 +6,11 @@ public class Constant {
     public final static String Redis_Index = "redis_index_obj";
     public final static String Redis_News = "redis_news";
     public final static String Redis_Activity = "redis_activity";
+
+    public static final String academyArtUrl = "https://www.caa.edu.cn/index.html";
+
+
+//    public static String imageStaticPath = "/Users/yuguangchao/Documents/workspace_git/tutuniao";
+    public static String imageStaticPath = "/data/static";
+
 }
--- src/test/java/com/tutuniao/tutuniao/schedule/ScheduledServiceTest.java
+++ src/test/java/com/tutuniao/tutuniao/schedule/ScheduledServiceTest.java
@@ -1,11 +1,18 @@
 package com.tutuniao.tutuniao.schedule;
 
 import com.alibaba.fastjson.JSONObject;
+import com.tutuniao.tutuniao.common.Constant;
 import com.tutuniao.tutuniao.entity.IndexObject;
+import com.tutuniao.tutuniao.util.HttpClientUtils;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ScheduledServiceTest {
 
     @Test
@@ -13,6 +20,8 @@ public class ScheduledServiceTest {
 //        IndexObject indexObject = new ScheduledService().buildIndex();
 //        System.out.println(JSONObject.toJSONString(indexObject));
 
-        System.out.println(ScheduledService.buildIndexJson());
+//        System.out.println(ScheduledService.buildIndexJson());
+        String dataAsStringFromUrl = HttpClientUtils.doGet(Constant.academyArtUrl);
+        ScheduledService.downloadImage(dataAsStringFromUrl);
     }
 }
